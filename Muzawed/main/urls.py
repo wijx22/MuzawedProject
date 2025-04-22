fa6caffcb02b825d64a26019ef5c073eb9027e3a
unknown <raghad.alqarni601@gmail.com>
2025-04-22 13:39:40 +0300

    a

--- Muzawed/main/urls.py
+++ Muzawed/main/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
+
+app_name = 'main'
 
 urlpatterns = [
+    path('', views.index_view , name='index_view'),
 ]
--- Muzawed/main/views.py
+++ Muzawed/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+def index_view(request):
+    return render(request, 'main/index.html')
 
 # Create your views here.
