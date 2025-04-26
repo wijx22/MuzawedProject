699eb85f156d6565d8feabc19df41296193eb1eb
Sabreen <sabrmbins@gmail.com>
2025-04-27 01:47:22 +0300

    order model

--- Muzawed/order/views.py
+++ Muzawed/order/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
 # Create your views here.
+from .models import Product
+
+def cart_view(request:HttpRequest):
+    items =Product.objects.all()
+    return render(request, "order/cart.html",{"items":items})
