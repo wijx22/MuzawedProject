5295b4c0fd65e0d96f4c60a6be82794f7b7c4c25
Sabreen <sabrmbins@gmail.com>
2025-04-29 09:18:00 +0300

    supplier modifications

--- Muzawed/supplier/forms.py
+++ Muzawed/supplier/forms.py
@@ -1,32 +1,32 @@
 from django import forms
 
-from .models import Branch, Supplier, SupplyRequest, CommercialInfo
+from .models import City, SupplyDetails, SupplyRequest, CommercialInfo
 
 
-class SupplierForm(forms.ModelForm):
+# class SupplierForm(forms.ModelForm):
+#     class Meta:
+#         model = SupplyDetails
+#         fields = [
+#             "logo",
+#             "supply_sector",
+#             "delivery_service",
+#             "order_lead_time_days",
+#             "is_available",
+#             "unavailable_from",
+#             "unavailable_to",
+#             "fast_service_details",
+#             "late_payment_options",
+#             "supply_days",
+#         ]
+#         widgets = {
+#             "unavailable_from": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+#             "unavailable_to": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+#         }
+
+
+class CitiesForm(forms.ModelForm):
     class Meta:
-        model = Supplier
-        fields = [
-            "logo",
-            "supply_sector",
-            "delivery_service",
-            "order_lead_time_days",
-            "is_available",
-            "unavailable_from",
-            "unavailable_to",
-            "fast_service_details",
-            "late_payment_options",
-            "supply_days",
-        ]
-        widgets = {
-            "unavailable_from": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "unavailable_to": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-        }
-
-
-class BranchForm(forms.ModelForm):
-    class Meta:
-        model = Branch
+        model = City
         fields = ["city"]
 
 
