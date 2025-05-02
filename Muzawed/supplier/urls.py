b5ab745613958d997a602b547075f799682a13b8
Sabreen <sabrmbins@gmail.com>
2025-05-02 18:19:50 +0300

    supply request

--- Muzawed/supplier/urls.py
+++ Muzawed/supplier/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("supplier-details/", views.supplier_details, name="supplier_details"),
     path("add_city/", views.add_city_view, name="add_city_view"),
     path('delete_city/<int:city_id>/', views.delete_city_view, name='delete_city_view'),
-    path("create/request/", views.create_supply_request,  name='create_supply_request'),
+    path("create/request/", views.supply_request_view,  name='supply_request_view'),
     path('supply_details/', views.supply_details_view, name='supply_details_view'),
     path('commercial_data/', views.commercial_data_view, name='commercial_data_view')
 ]
