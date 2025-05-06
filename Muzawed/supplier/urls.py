b0b5140ac7c3cad4c5e85f5eadb1957e81231966
wijx22 <wjdanalbdalzyz@gmail.com>
2025-05-07 01:00:09 +0300

    supplier report

--- Muzawed/supplier/urls.py
+++ Muzawed/supplier/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     path('update/commercial_data/', views.update_commercial_data_view, name='update_commercial_data_view'),
     path('store_info/', views.store_info_view, name='store_info_view'),
     path('store/', views.store_view, name='store_view'),
+    
+    path("dashboard/reports/", views.supplier_reports_dashboard, name="reports_dashboard"),
+    path("dashboard/reports/revenue/", views.report_revenue_by_product, name="report_revenue"),
+    path("dashboard/reports/user-behavior/", views.report_user_behavior, name="report_user_behavior"),
+    path("dashboard/reports/product-performance/", views.report_product_performance, name="report_product_performance"),
+    
 ]
