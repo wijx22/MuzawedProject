09f35ce22d922ba59f183861bfdce0d62da1f26c
wijx22 <wjdanalbdalzyz@gmail.com>
2025-05-06 12:37:59 +0300

    paths of support

--- Muzawed/support/urls.py
+++ Muzawed/support/urls.py
@@ -5,12 +5,7 @@ from . import views
 app_name = "support"
 
 urlpatterns = [
+    path("reports/", views.view_reports, name="view_report"),
     path("reports/create/", views.create_report, name="create_report"),
-
-
-    path(
-        "reports/<int:report_id>/replies/",
-        views.view_report_replies,
-        name="view_report_replies",
-    )
+    path("reports/<int:report_id>/replies/", views.view_report_replies, name="view_report_replies"),
 ]
