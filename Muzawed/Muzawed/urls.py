41429040c404915b2d601d813b450be99f0dc2d0
wijx22 <wjdanalbdalzyz@gmail.com>
2025-05-07 14:33:21 +0300

    setting

--- Muzawed/Muzawed/urls.py
+++ Muzawed/Muzawed/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('administration/', include('administration.urls')),
     path('payment/', include('payment.urls')),
     path('support/', include('support.urls'))
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
